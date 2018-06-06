eec3d1b6dbe73649bc0c344957085892334c898e
spdskatr <tunan.shi@gmail.com>
2018-06-06 19:58:31 +1000

    More dm triggers

---
+++ Wolfy/Commands/Workers/CommandCondition_NotInDms.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+
+namespace Wolfy.Commands.Workers
+{
+    public class CommandCondition_NotInDms : CommandCondition
+    {
+        public override bool CanExecute(MessageCreateEventArgs e)
+        {
+            return !(e.Channel is DiscordDmChannel);
+        }
+    }
+}
---
+++ Wolfy/Commands/Workers/CommandCondition_OnlyInDms.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Entities;
+
+namespace Wolfy.Commands.Workers
+{
+    public class CommandCondition_OnlyInDms : CommandCondition
+    {
+        public override bool CanExecute(MessageCreateEventArgs e)
+        {
+            return e.Channel is DiscordDmChannel;
+        }
+    }
+}
